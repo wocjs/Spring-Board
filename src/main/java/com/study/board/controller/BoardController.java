a45701e250c8f46713841c0ad3d8f3100ae22924
wocjs <wocjs602@gmail.com>
2023-09-07 17:56:07 +0900

    d

---
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,68 @@
+package com.study.board.controller;
+
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write") // localhost:8080/board/write
+    public String boardWriteForm(){
+
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board){
+        System.out.println(board.getTitle());
+        boardService.write(board);
+
+        return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAttribute("list", boardService.boardList());
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view")  // localhost:8080/board/view?id={id}
+    public String boardview(Model model, Integer id){
+        model.addAttribute("board", boardService.boardView((id)));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boarddelete(Integer id){
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}")
+    public String boardmodify(@PathVariable("id") Integer id, Model model){
+
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardupdate(@PathVariable("id") Integer id, Board board){
+        Board boardtmp = boardService.boardView(id);
+        boardtmp.setTitle(board.getTitle());
+        boardtmp.setContent(board.getContent());
+
+        boardService.write(board);
+
+        return "redirect:/board/list";
+    }
+}
